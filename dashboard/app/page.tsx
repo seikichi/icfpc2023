7ebb600d5fc33e0ddb6cb3d1b6ba0bc602d7d7fe
Seikichi KONDO <seikichi@kmc.gr.jp>
2023-07-05 01:20:14 +0900

    Add prisma and db settings to dashboard

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <>Hello, world!</>;
+import { prisma } from "@/lib/db";
+import logger from "@/lib/logger";
+
+export const revalidate = 0;
+
+export default async function Home() {
+  // NOTE: just for debugging prisma and logger...
+  const count = await prisma.problem.count();
+  logger.info({ message: `# of problems: ${count}` });
+
+  return <>Hello, world! (# of problems: {count})</>;
 }
