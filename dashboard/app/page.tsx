72bec4f134a469e3fc18bd9edd11b4d2f2f3a106
Seikichi KONDO <seikichi@kmc.gr.jp>
2023-07-05 04:27:52 +0900

    Add tremor UI

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -1,12 +1,33 @@
+import { Card, Title, Text } from "@tremor/react";
 import { prisma } from "@/lib/db";
 import logger from "@/lib/logger";
+import UsersTable from "@/components/UserTable";
 
-export const revalidate = 0;
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   // NOTE: just for debugging prisma and logger...
   const count = await prisma.problem.count();
   logger.info({ message: `# of problems: ${count}` });
 
-  return <>Hello, world! (# of problems: {count})</>;
+  const users = [
+    {
+      id: 1,
+      name: "Seiichi KONDO",
+      username: "seikichi",
+      email: "seikichi@kmc.gr.jp",
+    },
+  ];
+
+  return (
+    <main className="p-4 md:p-10 mx-auto max-w-7xl">
+      <Title>Users</Title>
+      <Text>
+        A list of users retrieved from a MySQL database (PlanetScale).
+      </Text>
+      <Card className="mt-6">
+        <UsersTable users={users} />
+      </Card>
+    </main>
+  );
 }
