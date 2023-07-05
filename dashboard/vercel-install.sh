4f5596517f18a9433a73d7c9310a508aed32eb3d
Seikichi KONDO <seikichi@kmc.gr.jp>
2023-07-06 03:37:11 +0900

    Fix vercel

--- dashboard/vercel-install.sh
+++ dashboard/vercel-install.sh
@@ -6,10 +6,16 @@ set -eux
 pushd ../wasm
 
 # NOTE: should we specify --target option? (default: bundler)
-amazon-linux-extras install rust1
-rustup target add wasm32-unknown-unknown
-cargo install wasm-pack
+echo "Installing Rustup..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
+
+echo "Installing wasm-pack..."
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+
+echo "Build WASM"
 /vercel/.cargo/bin/wasm-pack build --release
+
 popd
 
 # The following command is default install command...
