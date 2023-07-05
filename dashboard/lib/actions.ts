c019759171f4f6270a813a9e6252d2b0a7020124
Seikichi KONDO <seikichi@kmc.gr.jp>
2023-07-06 01:14:57 +0900

    Specify AWS region for Lambda client

--- dashboard/lib/actions.ts
+++ dashboard/lib/actions.ts
@@ -5,7 +5,7 @@ import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
 import { env } from "@/lib/env";
 
 export async function invokeSolver() {
-  const client = new LambdaClient({});
+  const client = new LambdaClient({ region: env.AWS_DEFAULT_REGION });
   const command = new InvokeCommand({ FunctionName: env.SOLVER_LAMBDA_ARN });
   await client.send(command);
 }
