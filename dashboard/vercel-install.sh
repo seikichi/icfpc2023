9505bc8bf1363c9045f3c6cd8252fd9a97c6f6dd
Seikichi KONDO <seikichi@kmc.gr.jp>
2023-07-06 03:25:41 +0900

    Fix Vercel setup script

--- dashboard/vercel-install.sh
+++ dashboard/vercel-install.sh
@@ -8,7 +8,7 @@ pushd ../wasm
 # NOTE: should we specify --target option? (default: bundler)
 amazon-linux-extras install rust1
 cargo install wasm-pack
-/vercel/.cargo/bin/wasm-pack build --release
+/vercel/.cargo/bin/wasm-pack build --release --target bundler
 popd
 
 # The following command is default install command...
