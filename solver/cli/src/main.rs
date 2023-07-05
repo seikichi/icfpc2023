8bfbe3444603911ab8c341e068ba9381b0048e03
Seikichi KONDO <seikichi@kmc.gr.jp>
2023-07-05 18:54:32 +0900

    Add solver directory

---
+++ solver/cli/src/main.rs
@@ -0,0 +1,5 @@
+extern crate core;
+
+fn main() {
+    println!("Answer to the Ultimate Question: {}", core::add(40, 2));
+}
---
+++ solver/core/src/lib.rs
@@ -0,0 +1,4 @@
+// NOTE: 動作確認用
+pub fn add(a: u64, b: u64) -> u64 {
+    a + b
+}
