fb73089fc83a70f0c04c7a4918ea79df1e80f976
MORI Shingo <cos@kmc.gr.jp>
2023-07-08 10:43:29 +0900

    allow dead_code

--- solver/core/src/output.rs
+++ solver/core/src/output.rs
@@ -1,11 +1,13 @@
 use super::*;
 use std::{io, path::Path};
 
+#[allow(dead_code)]
 pub fn load_from_file<P: AsRef<Path>>(path: P) -> io::Result<Solution> {
     let s = std::fs::read_to_string(path)?;
     load_from_str(&s)
 }
 
+#[allow(dead_code)]
 pub fn load_from_str(s: &str) -> io::Result<Solution> {
     let solution: Solution = serde_json::from_str(s)?;
     Ok(solution)
